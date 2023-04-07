7bcd48fe6d54f8c6ef95f0e10ebf795f9afdb104
nahidhasan141400 <nahidhasan141400@gmail.com>
2023-04-07 16:16:36 +0600

    add new

---
+++ Team/Hira/js/main.js
@@ -0,0 +1,27 @@
+var app = document.getElementById('type');
+
+
+var typewriter = new Typewriter(app, {
+    loop: true,
+    delay: 100,
+  });
+
+
+  typewriter.pauseFor(200)
+  .typeString('<strong>Chief Design Officer</strong> of Paradise Solution ')
+  .pauseFor(1200)
+  .deleteChars(42)
+  .typeString('a <strong>Graphic Designer</strong>')
+  .pauseFor(1200)
+  .deleteChars(18)
+  .typeString(' <strong>Professional Photographer</strong>')
+  .pauseFor(1000)
+  .deleteChars(27)
+  .typeString('a <strong>Digital Marketar</strong> ')
+  .pauseFor(1000)
+  .deleteChars(28)
+  .typeString('an <strong>Electrical Engineer</strong>')
+  .pauseFor(5000)
+  .start();  
+
+  console.log("nahid")
